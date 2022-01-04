62d381549f73228543bce6619e97a148977922a9
fangyihaoo <fangyihao116@gmail.com>
2022-01-04 18:57:34 -0500

    Add airbnb blog

--- facebook.sql
+++ facebook.sql
@@ -108,21 +108,24 @@ Q3.What is percentage of sender without fraud in all senders?
   )
   as fraud_rate
   
-Product round:
+Product sense:
+  https://medium.com/airbnb-engineering/at-airbnb-data-science-belongs-everywhere-917250c6beba
 ---------
 You are a DS on the friending team. The overall number of friend accepts on the platform has gone down by 5% in June. How would you look into this?
-  1. Check the data first to see if it is edogenous or exogenous. We can look into historical date to see if there is a seasonal pattern.
+  idea: break down the problem.
+  1. Check the data collection firstly, how did we collect the data? Any seasonal pattern? Special events?
   2. Check the overall number of friend request to see if the it drops or not. If drops, the issue may comes from the friend request side, then we can check the 
   add friend button to see if there is any changes around it before June. If yes, we can check it CTR for add friends button and if there is a deline then it might 
   be the reason.
   3. New feature launched, primacy effect.
-  4. also cohort users based on platform they are using and also the region, to see if it is platform specific or region specific decline.
+  4. also cohort users based on platform they are using and also the region (geography information), to see if it is platform specific or region specific decline.
 ---------
 We have a suggestion to add more relatives in the friend algorithm. How would you test if this is a good or a bad idea?
-  1. The goal of this new feature is to increase the number of user friends, to increase the users engagement.
-  2. The metircs we propose are to check two things. 1. if the new features work or not. 2. the long term goal like increasing engagement.
-  3. For the first one, we can use CTR of requests send, or friends added, especially for those who interact with the user within one day (effecive friend).
-  For the second one, we can use DAU or time spent. 
-  4. Tradeoff to measure (number of relatives) (best metric, accepted friend request per day)
+  1. What is the goal of this new feature? Find metrics like friends requests/accept rate, Monthly active users, user engagement(posts,comments,shares,likes) meaningful connection.
+  2. The metircs we propose are to check two things. 1. if the new features work or not (A/B testing). 2. bring positive effects in long term(user engagament, revenue)
+  3. For the first one, we can propose the A/B test on those metrics (CTR of friend require/accept button, or CTP of users) Bern(p), design experiment. 
+For the second one, before A/B testing, we need to identify opportunit, what is the opportunity sizing? For specific user or all user? 
+  4. Tradeoff between metrics, like month active users increase but engagement decrease in a short time, but user engagement could increase in a long period since we have more MAU.
+  If both of them increase, we can consider the revenue it can bring to us by the opportunity sizing of target group.
 
 
